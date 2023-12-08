38a5cf83372dbaf76c6f0239b9a7f5cf4faac274
eduardsdn <eduard.sdn@gmail.com>
2023-12-08 12:50:55 +0100

    Bot now replies with voice messages

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ const audioGen = async function (textForTranslation) {
     voice: "alloy",
     input: textForTranslation,
   });
-  pathToSpeechFile = `${uuidv4()}.mp3`;
+  pathToSpeechFile = `./audio/${uuidv4()}.mp3`;
   const buffer = Buffer.from(await audio.arrayBuffer());
   await fs.promises.writeFile(pathToSpeechFile, buffer);
   return pathToSpeechFile;
@@ -47,18 +47,27 @@ bot.on("message", async (ctx) => {
   const userMessage = ctx.update.message.text;
   console.log(userMessage);
   textGen(
-    `You are an art guide named Gallery Genius, your goal is to provide fascinating information about paintings. Users will tell you the name, author and year of the painting and based on this data you will find the painting and interesting information related to it. You will act as a friendly tour guide passionate about art and his job. If a user provides you with something but not the name of the painting, the name of the author or the year of creation, you will ALWAYS write a short message asking the user for the artist's name and title to assist in the search, and not answer questions that are not related to art. If you do not have any information about that painting or you are not sure about the information you will tell it to the user. Your goal is to educate and enrich the user's understanding of art with thorough and accurate information, You will use information from such sources as Google Arts & Culture, Art Institute of Chicago, Artcyclopedia, WikiArt, National Gallery of Art Collection Search, National Gallery of Art Open Access Images. Here is data from the user${userMessage}`
+    `Вы - гид по искусству под названием Gallery Genius, ваша цель - предоставить увлекательную информацию о картинах. Пользователи сообщат вам название, автора и год создания картины на разных языках, и на основе этих данных вы найдете картину и интересную информацию, связанную с ней. Вы будете выступать в роли дружелюбного гида, увлеченного искусством и своей работой. Если пользователь предоставляет вам что-то, но не название картины, имя автора или год создания, вы всегда будете писать короткое сообщение с просьбой указать имя и должность художника, чтобы помочь в поиске, и не отвечать на вопросы, которые не связаны с искусством. Если у вас нет никакой информации об этой картине или вы не уверены в достоверности информации, вы сообщите ее пользователю. Ваша цель - обучить и обогатить понимание искусства пользователем с помощью подробной и точной информации, в приоритете вы будете использовать информацию из таких источников, как Google Arts & Culture, Чикагский институт искусств, Artcyclopedia, WikiArt, Поиск коллекций Национальной галереи искусств, изображения открытого доступа Национальной галереи искусств, однако вы также можете использовать и другие источники. Вы ответите на языке, который использовался пользователем. Вот данные от пользователя:${userMessage}`
   ).then((response) => {
     audioGen(response).then((response) => {
       console.log(response);
-      ctx.telegram.sendAudio(ctx.chat.id, {
-        source: `./${response}`,
-        filename: "Your information.mp3",
-      });
+      const filePath = `./${response}`;
+      ctx.telegram.sendVoice(ctx.chat.id, { source: filePath });
     });
   });
-  // .then(ctx.replyWithAudio({ source: pathToSpeechFile }));
-  // ctx.reply(response)
 });
 
 bot.launch();
+
+// ctx.telegram.sendAudio(ctx.chat.id, {
+//   source: `./${response}`,
+//   filename: "Your information.mp3",
+// });
+
+// fs.unlink(filePath, (err) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log("File is deleted.");
+//   }
+// });
